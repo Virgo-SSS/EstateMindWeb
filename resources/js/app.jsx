07734cb05513bbec59e68a4f566240a3dcbb54d6
virgo <virgostevanus27@gmail.com>
2025-03-05 18:05:19 +0700

    change resolve component from eager to lazy using laravel vite plugin

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,15 +1,13 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import '../css/app.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => appName,
-    resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-        return pages[`./Pages/${name}.jsx`]
-    },
+    title: () => appName,
+    resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />)
     },
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'EstateMind') }}</title>
 
     @viteReactRefresh
     @vite('resources/js/app.jsx')
