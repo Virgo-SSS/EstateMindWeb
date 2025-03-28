df319744d963349460a551a6b65a09c9b1a0ab84
virgo <virgostevanus27@gmail.com>
2025-03-28 20:14:11 +0700

    fix project input not reseted after close create project modal

--- resources/js/Pages/Projects/Projects.jsx
+++ resources/js/Pages/Projects/Projects.jsx
@@ -23,6 +23,11 @@ export default function Projects({ projects }) {
         });
     }
 
+    const handleModalClose = () => {
+        reset();
+        closeModal();
+    }
+
     return (
         <>
             <AppLayout>
@@ -47,7 +52,7 @@ export default function Projects({ projects }) {
                 </div>
             </AppLayout>
 
-            <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[500px] m-4">
+            <Modal isOpen={isOpen} onClose={handleModalClose} className="max-w-[500px] m-4">
                 <div className="no-scrollbar relative w-full max-w-[500px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
                     <div className="px-2 pr-14">
                         <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
@@ -77,7 +82,7 @@ export default function Projects({ projects }) {
                         </div>
 
                         <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
-                            <Button size="sm" variant="outline" onClick={closeModal}>
+                            <Button size="sm" variant="outline" onClick={handleModalClose}>
                                 Close
                             </Button>
 
