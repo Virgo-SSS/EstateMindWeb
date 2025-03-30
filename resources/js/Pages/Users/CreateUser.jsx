75a973ffc5d245af78559c2dc223b2a5d37f449c
virgo <virgostevanus27@gmail.com>
2025-03-30 15:39:07 +0700

    refactor input and select component, add create user logic

--- resources/js/Pages/Users/CreateUser.jsx
+++ resources/js/Pages/Users/CreateUser.jsx
@@ -1,10 +1,185 @@
+import { EyeIcon, EyeOff, LoaderCircle } from "lucide-react";
+import Button from "../../components/ui/button/Button";
+import Input from "../../components/ui/input/Input";
+import Select from "../../components/ui/input/Select";
+import Label from "../../components/ui/label/Label";
 import AppLayout from "../../layout/AppLayout";
+import { useState } from "react";
+import { useForm, usePage } from "@inertiajs/react";
 
 export default function CreateUser() {
+    const [ showPassword, setShowPassword ] = useState(false)
+    const pageProps = usePage().props
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: "",
+        email: "",
+        password: "",
+        is_super_admin: "",
+    });
+
+    const handleSave = (e) => {
+        e.preventDefault();
+
+        if (!checkRequiredFields()) {
+            setData({
+                ...data,
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                is_super_admin: data.is_super_admin,
+            })
+            
+            return;
+        }
+
+        post(route("users.store"), {
+            onSuccess: () => {
+                reset();
+            },
+            onError: (err) => {
+                // TODO: Handle error case (e.g., show a toast notification)
+                console.log("Failed to create user:", err);
+            }
+        });
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (errors[name]) {
+            delete errors[name];
+        }
+        setData(name, value);
+    }
+
+    const checkRequiredFields = () => {
+        const requiredFields = ["name", "email", "password", "is_super_admin"];
+        let allFieldsFilled = true;
+
+        requiredFields.forEach(field => {
+            if (!data[field]) {
+                let errorMessage = `${field.charAt(0).toUpperCase() + field.slice(1)} is required`;
+                if(field === "is_super_admin") {
+                    errorMessage = "Role is required";
+                }
+
+                errors[field] = errorMessage;
+                allFieldsFilled = false;
+            } else {
+                delete errors[field];
+            }
+        });
+
+        return allFieldsFilled;
+    }
+
     return (
         <>
             <AppLayout>
-                <h1>Create User</h1>
+                <div className="flex flex-wrap items-center justify-between gap-3 mb-6">
+                <div className="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03] w-2/3">
+                    <div className="px-6 py-5">
+                        <h3 className="text-base font-medium text-gray-800 dark:text-white/90">
+                            Create User
+                        </h3>
+                    </div>
+                    <div className="p-4 border-t border-gray-100 dark:border-gray-800 sm:p-6">
+                        <div className="space-y-6">
+                            <form onSubmit={handleSave}>
+                                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                                    <div>
+                                        <Label htmlFor="name">
+                                            Name <span className="text-error-500">*</span>{" "}
+                                        </Label>
+                                        <Input
+                                            id="name"
+                                            name="name"
+                                            placeholder="Name"
+                                            isRequired={true}
+                                            value={data.name}
+                                            onChange={handleChange}
+                                            error={errors.name ? true : false}
+                                            hint={errors.name}
+                                        />
+                                    </div>
+                                    <div>
+                                        <Label htmlFor="email">
+                                            Email <span className="text-error-500">*</span>{" "}
+                                        </Label>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            name="email"
+                                            isRequired={true}
+                                            placeholder="Email address"
+                                            value={data.email}
+                                            onChange={handleChange}
+                                            error={errors.email ? true : false}
+                                            hint={errors.email}
+                                        />
+                                    </div>
+                                    <div>
+                                        <Label htmlFor="password">
+                                            Password <span className="text-error-500">*</span>{" "}
+                                        </Label>
+                                        <Input
+                                            id="password"
+                                            name="password"
+                                            type={showPassword ? "text" : "password"}
+                                            placeholder="Password"
+                                            isRequired={true}
+                                            icon={showPassword ? (
+                                                <EyeIcon className="dark:text-white" />
+                                            ) : (
+                                                <EyeOff className="dark:text-white" />
+                                            )}
+                                            iconOnClick={() => setShowPassword(!showPassword)}
+                                            value={data.password}
+                                            onChange={handleChange}
+                                            error={errors.password ? true : false}
+                                            hint={errors.password}
+                                        />
+                                    </div>
+                                    <div>
+                                        <Label htmlFor="is_super_admin">
+                                            Role <span className="text-error-500">*</span>{" "}
+                                        </Label>
+                                        <Select
+                                            id="is_super_admin"
+                                            name="is_super_admin"
+                                            required={true}
+                                            placeholder="Role"
+                                            options={[
+                                                { label: "Select Role", value: "", disabled: true },
+                                                { label: "Super Admin", value: 1 },
+                                                { label: "Admin", value: 0 },
+                                            ]}
+                                            value={data.is_super_admin}
+                                            onChange={handleChange}
+                                            error={errors.is_super_admin ? true : false}
+                                            hint={errors.is_super_admin}
+                                        />
+                                    </div>
+                                    <div className="col-span-full">
+                                        {
+                                            pageProps.flash.success && (
+                                                <div className="text-sm text-green-500 mb-2">
+                                                    {pageProps.flash.success}
+                                                </div>
+                                            )
+                                        }
+                                        <Button
+                                            size="sm"
+                                            disabled={processing}
+                                        >
+                                            Create User
+                                        </Button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </div>
             </AppLayout>
         </>
     );
--- resources/js/components/ui/input/Select.jsx
+++ resources/js/components/ui/input/Select.jsx
@@ -1,18 +1,69 @@
-export default function Select({ options, value, onChange, placeholder, className }) {
+export default function Select({ 
+    id,
+    name,
+    options, 
+    value,
+    onChange,
+    placeholder,
+    className,
+    disabled = false,
+    required = false,
+    hint,
+    error = false,
+    success = false,
+}) {
+    const disabledOptionClass = 'text-gray-700 dark:text-gray-300 bg-gray-200 dark:bg-gray-600';
+    const enabledOptionClass = 'text-gray-700 dark:text-gray-400 dark:bg-gray-900';
+
+    let inputClasses = `w-full h-11 appearance-none rounded-lg border px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 pr-11 ${className}`
+
+    if (disabled) {
+        inputClasses += ` text-gray-500 border-gray-300 opacity-40 bg-gray-100 cursor-not-allowed dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 opacity-40`
+    } else if (error) {
+        inputClasses += ` border-error-500 focus:border-error-300 focus:ring-error-500/20 dark:text-error-400 dark:border-error-500 dark:focus:border-error-800`
+    } else if (success) {
+        inputClasses += `  border-success-500 focus:border-success-300 focus:ring-success-500/20 dark:text-success-400 dark:border-success-500 dark:focus:border-success-800`
+    } else {              
+        inputClasses += ` bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-brand-500/20 dark:border-gray-700 dark:text-white/90  dark:focus:border-brand-800`
+    }
+
     return (
-        <select
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-            className={`w-60 h-11 appearance-none rounded-lg border px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 text-gray-400 border-gray-300 bg-transparentpr-11focus:border-brand-300  focus:ring-brand-500/10 dark:border-gray-700 ${className}`} 
-        >
-            {options.map((option, index) => (
-                <option key={index} value={option.value} className="text-gray-700 dark:bg-gray-900 dark:text-gray-400">
-                    {option.label}
-                </option>
-            ))}
-        </select>
-            
-        
+        <>
+            <select
+                disabled={disabled}
+                id={id}
+                name={name}
+                required={required}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className={inputClasses}
+            >
+                {options.map((option, index) => (
+                    <option 
+                        key={index} 
+                        value={option.value} 
+                        disabled={option.disabled}
+                        className={`${option.disabled ? disabledOptionClass : enabledOptionClass}`}
+                    >
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+
+            {hint && (
+                <p
+                    className={`mt-1.5 text-xs ${
+                        error
+                        ? "text-error-500"
+                        : success
+                        ? "text-success-500"
+                        : "text-gray-500"
+                    }`}
+                >
+                    {hint}
+                </p>
+            )}
+        </>
     )
 }
